7e9d95f429fa8a3fd712626f2faed0a1b4612735
Super User <souravshrestha22@gmail.com>
2025-07-15 11:48:36 +0530

    adding a file name file.sh which contains use of -x and -f to check if the file is present and executable or not

---
+++ file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "enter the name of the file: " a
+
+if [ -f "$a" ] && [ -x "$a" ] ;then
+	echo "file is present ans executable"
+elif [ -f "$a" ]; then
+	echo "file is  present but not executeable"
+else
+	echo "file does not exist"
+fi
